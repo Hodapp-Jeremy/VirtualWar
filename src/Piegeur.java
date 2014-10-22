2b831349a2b4fd0612658ea459ed3ecb5b4dba95
Clement Cattelle <gamisensei@gmail.com>
2014-10-22 18:42:26 +0200

    Debut javadoc

--- src/Piegeur.java
+++ src/Piegeur.java
@@ -3,57 +3,69 @@ import java.util.List;
 
 
 public class Piegeur extends Robot{
+	
 	/**
-	 * Portee : 1
-	 * Energie : 50
-	 * Cout Deplacement: 2
-	 * Deplacement : 1case
-	 * Cout Mine : 2
-	 * Regeneration Base : +2/tour
-	 * Degat Tir/Mine :  -2
+	 * 
+	 * @param vue 
+	 * @param l
+	 * @param h
+	 * @param equipe
 	 */
-	public Piegeur(Vue vue, int l, int h, int equipe) {
-		super(vue, l, h, equipe);
+	public Piegeur(Vue vue, int largeur, int hauteur, int equipe) {
+		super(vue, largeur, hauteur, equipe);
 		this.setEnergie(50);
 	}
 
-	
+	/**
+	 * @return true si le robot peut poser sa mine, sinon return false
+	 * 
+	 */
 	public boolean peutTirer() {
 		
 		return false;
 	}
 
-	
+	/**
+	 * @return 2 , le cout d'une action pour un piegeur
+	 */
 	public int getCoutAction() {
 		
 		return 2;
 	}
 
-	
+	/**
+	 * @return 2, le cout d'un deplacement pour un piegeur
+	 */
 	public int getCoutDep() {
 		
 		return 2;
 	}
 
-	
+	/**
+	 * @return 2, l'energie qu'un piegeur perd en essuyant un tir ennemi
+	 */
 	public int getDegatTir() {
 		
 		return 2;
 	}
 
-	
+	/**
+	 * @return 2, l'energie qu'un piegeur perd en marchant sur une mine
+	 */
 	public int getDegatMine() {
-		
 		return 2;
 	}
 
-	
+	/**
+	 * return le type de robot
+	 */
 	public String getType() {
-		
 		return "Piegeur";
 	}
 
-	
+	/**
+	 * euh...
+	 */
 	public List<Coordonnees> getDeplacements() {
 		
 		return null;
